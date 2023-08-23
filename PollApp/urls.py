65537d8c55a5350ed4d38457fa5d3781cc51bf9e
Fukulu <furkantumay@gmail.com>
2023-08-23 20:01:09 +0300

    anasayfa html tasarımı yapıldı...

--- PollApp/urls.py
+++ PollApp/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = "PollApp"
 
 urlpatterns = [
-    path("", views.home_view, name="home"),
+    path("", views.index_view, name="index"),
+    path("home", views.home_view, name="home"),
     path("poll/<int:poll_id>/", views.poll_view, name="poll"),
     path("poll_result/<int:poll_id>/", views.poll_result_view, name="poll_result"),
 ]
--- PollApp/views.py
+++ PollApp/views.py
@@ -24,15 +24,11 @@ def poll_view(request, poll_id):
     return render(request, template_name="PollApp/poll.html", context={"poll": poll})
 
 
-#
-# Buraya sonuçları string olarak yazdıracak bir kod yazmaya çalıştım fakat yetiştiremedim
-# Örneğin: Python için 5 oy/Java için 10 oy/HTML için 3 oy kullanılmıştır gibi...
-# def poll_result_view(request, poll_id):
-#     poll = Poll.objects.get(id=poll_id)
-#     show_results = True  # Sonuçları göstermek için bir değişken
-#     return render(request, template_name="PollApp/poll.html", context={"poll": poll, "show_results": show_results})
-
 def poll_result_view(request, poll_id):
     poll = Poll.objects.get(id=poll_id)
     choices_with_votes = Choice.objects.filter(related_polls=poll).annotate(total_votes=Count('votes'))
     return render(request, template_name="PollApp/poll_result.html", context={"poll": poll, "choices_with_votes": choices_with_votes})
+
+
+def index_view(request):
+    return render(request, template_name="PollApp/index.html")

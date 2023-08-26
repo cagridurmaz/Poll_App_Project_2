9a01f9532ee30f29c732a4efdc69296ffec33382
cagridurmaz <cagridurmaz@gmail.com>
2023-08-27 02:14:42 +0300

    last_update

--- PollApp/views.py
+++ PollApp/views.py
@@ -12,7 +12,7 @@ def home_view(request):
     return render(request, template_name="PollApp/home.html", context={"polls": polls})
 
 
-@login_required
+@login_required(login_url='/users/login')
 def poll_view(request, poll_id):
     poll = Poll.objects.get(id=poll_id)
 
@@ -30,8 +30,15 @@ def poll_view(request, poll_id):
 def poll_result_view(request, poll_id):
     poll = Poll.objects.get(id=poll_id)
     choices_with_votes = Choice.objects.filter(related_polls=poll).annotate(total_votes=Count('votes'))
+
+    total_votes = sum(choice.total_votes for choice in choices_with_votes)
+
+    for choice in choices_with_votes:
+        choice.vote_percentage = (choice.total_votes / total_votes) * 100 if total_votes > 0 else 0
+
     return render(request, template_name="PollApp/poll_result.html",
                   context={"poll": poll, "choices_with_votes": choices_with_votes})
+
 
 
 def index_view(request):
